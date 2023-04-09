c0393e572773bad99a398c9438056d9a730be7a6
Yovan Julio Adam <yovanjulioadam@gmail.com>
2023-04-09 23:15:16 +0700

    add joi validator

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+const { NotePayloadSchema } = require('./schema');
+
+const NotesValidator = {
+  validateNotePayload(payload) {
+    const validateResult = NotePayloadSchema.validate(payload);
+    if (validateResult.error) {
+      throw new Error(validateResult.error.message);
+    }
+  },
+};
+
+module.exports = NotesValidator;
